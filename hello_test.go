8664323bbf588ed04310b0ab6729fef860f4e5b3
Christian Aracena <cdaracena@Christians-MacBook-Pro.local>
2020-07-04 23:05:50 -0400

    added test helper function, added additional tests

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-  got := Hello("Christian")
-  want := "Hello, Christian"
 
-  if got != want {
-    t.Errorf("got %q want %q", got, want)
+  assertCorrectMessage := func(t *testing.T, got, want string) {
+    t.Helper()
+    if got != want {
+      t.Errorf("got %q want %q", got, want)
+    }
   }
+
+  t.Run("Saying hello to ppl", func(t *testing.T) {
+    got := Hello("Christian")
+    want := "Hello, Christian"
+    assertCorrectMessage(t, got, want)
+  })
+
+  t.Run("Say 'Hello, World' when an empty string is provided", func(t *testing.T) {
+    got := Hello("")
+    want := "Hello, World"
+    assertCorrectMessage(t, got, want)
+  })
 }
